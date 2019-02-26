5cf1cf4c769ed76664d4f12c666934bed413ec8c
Łukasz Niewęgłowski <lnieweglowski@kcalmar.com>
2019-02-26 14:04:29 +0100

    removed main construction

--- lesson2_ex1.py
+++ lesson2_ex1.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 
-if __name__ == "__main__":
-    driver = webdriver.Firefox()
-    driver.get("https://www.google.com")
-    driver.close()    
+driver = webdriver.Firefox()
+driver.get("https://www.google.com")
+driver.close()    
