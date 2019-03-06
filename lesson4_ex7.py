6bbfb25d3db4d86b9d0b92300ed5c24069c7da57
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2019-03-06 22:21:23 +0100

    adding new file

---
+++ lesson4_ex7.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+
+class TestLitecartLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.get("http://localhost/litecart/admin/login.php")
+        cls.driver.find_element_by_name("username").send_keys("admin")
+        cls.driver.find_element_by_name("password").send_keys("admin")
+        cls.driver.find_element_by_name("remember_me").click()
+        cls.driver.find_element_by_name("login").click()
+
+    def test_appearence_category(self):
+        self.driver.find_element_by_xpath("//span[@text='Appearence']").click()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
