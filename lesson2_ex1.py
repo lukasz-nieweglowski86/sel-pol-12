7a83ac2e95ab8792159c92f9d365330c1bdcc5e9
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2019-03-06 22:09:38 +0100

    code refactor

--- lesson2_ex1.py
+++ lesson2_ex1.py
@@ -1,5 +1,18 @@
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("https://www.google.com")
-driver.quit()
+class TestLitecartLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+
+    def test_google_page(self):
+        self.driver.get("https://www.google.com")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
