e33a985ffbe8f9cb97418d1a4ade7cc042bb5a63
Łukasz Niewęgłowski <lnieweglowski@kcalmar.com>
2019-03-07 15:58:24 +0100

    l4ex8

--- lesson4_ex8.py
+++ lesson4_ex8.py
@@ -9,7 +9,13 @@ class TestLitecartMainPage(unittest.TestCase):
         cls.driver.get("http://localhost/litecart/en/")
 
     def test_count_stickers(self):
-        print(len(self.driver.find_elements_by_xpath("//div[contains(@class,'sticker')]")))
+        product_list = self.driver.find_elements_by_xpath("//li[contains(@class,'product')]")
+        for product in product_list:
+            sticker_count = len(product.find_elements_by_xpath("a/div/div[contains(@class,'sticker')]"))
+            if sticker_count == 1:
+                print("Product has only 1 sticker.")
+            else:
+                print("Product does not have only 1 sticker.")
 
     @classmethod
     def tearDownClass(cls):
