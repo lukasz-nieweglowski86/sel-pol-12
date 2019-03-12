b18be18406ef602aec7eb32ec28d7609e6fec347
Łukasz Niewęgłowski <lnieweglowski@kcalmar.com>
2019-03-12 15:37:09 +0100

    L5E9

--- lesson5_ex9_2.py
+++ lesson5_ex9_2.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
@@ -13,7 +14,7 @@ class TestLitecartAdminPage(unittest.TestCase):
         cls.driver.find_element_by_name("remember_me").click()
         cls.driver.find_element_by_name("login").click()
 
-    def test_geozones(self):
+    def test_sorting_geozones(self):
         self.driver.find_element_by_xpath("//span[text()='Geo Zones']").click()
         rows = self.driver.find_elements_by_xpath("//tr[@class='row']/td[3]")
 
@@ -24,29 +25,32 @@ class TestLitecartAdminPage(unittest.TestCase):
 
         for country in countries_names:
             self.driver.find_element_by_xpath("//a[text()='%s']" % country).click()
-            zones_list = self.driver.find_elements_by_xpath("//tr/td[3]/select[contains(@name,'zones')]")
+            print("Checking %s. Please wait..." % country)
+            time.sleep(5)
+            zones_list = []
+            if country == "European Union":
+                zones_list = self.driver.find_elements_by_xpath("//tr/td[2]/select[contains(@name,'zones')]/option[@selected='selected']") 
+            else:
+                zones_list = self.driver.find_elements_by_xpath("//tr/td[3]/select[contains(@name,'zones')]/option[@selected='selected']")
 
             zones_names = []
 
-            select = Select(self.driver.find_element_by_xpath("//table[@id='table-zones']/tbody/tr/td[3]/select"))
-            selected_option = select.first_selected_option.get_attribute("textContent")
-            print(selected_option)
-            
-            # for zone in zones_list:
-            #      zones_names.append(zone.text)
+            for zone in zones_list:
+                zones_names.append(zone.text)
+                print(zone.text)
 
-            # zones_names_sorted = zones_names[:]
-            # zones_names_sorted.sort()
+            zones_names_sorted = zones_names[:]
+            zones_names_sorted.sort()
 
-            # if zones_names == zones_names_sorted:
-            #     print("Zones in %s are sorted alphabetically." % country)
-            # else:
-            #     print("Zones in %s are not sorted alphabetically." % country)
+            if zones_names == zones_names_sorted:
+                print("Zones in %s are sorted alphabetically." % country)
+            else:
+                print("Zones in %s are not sorted alphabetically." % country)
             self.driver.back()
         
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
