fc9392a0b200b37301a75944a80650c39d1ae38b
Łukasz Niewęgłowski <lnieweglowski@kcalmar.com>
2019-03-20 13:59:57 +0100

    minor changes

--- lesson8_ex14.py
+++ lesson8_ex14.py
@@ -16,21 +16,26 @@ class TestLitecartMainPage(unittest.TestCase):
         cls.driver.find_element_by_name("remember_me").click()
         cls.driver.find_element_by_name("login").click()
 
-    def test_frames_and_windows(self):
-        # opening 'Countries' tab 
+    def test_windows(self):
+        wait = WebDriverWait(self.driver, 10)
+        
         self.driver.find_element_by_xpath("//span[text()='Countries']").click()
-        # adding new country
+        
         self.driver.find_element_by_xpath("//a[@class='button']").click()
         
-        links_list = self.driver.find_elements_by_xpath("//i[@class='fa fa-external-link']")
+        links_list = self.driver.find_elements_by_xpath("//a[i[@class='fa fa-external-link']]")
+        main_window = self.driver.current_window_handle
+        old_windows = self.driver.window_handles
         for link in links_list:
             link.click()
-            
+            wait.until(EC.new_window_is_opened(old_windows))
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.driver.close()
+            self.driver.switch_to.window(main_window)
 
-
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
