74b651a3def0408a940d7aa7a6ac7151cf4f0288
Łukasz Niewęgłowski <lnieweglowski@kcalmar.com>
2019-03-28 16:01:12 +0100

    ex update

--- lesson10_ex17.py
+++ lesson10_ex17.py
@@ -2,12 +2,15 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
 
 class TestLitecartMainPage(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
         cls.driver.get("http://localhost/litecart/admin/login.php")
         cls.driver.find_element_by_name("username").send_keys("admin")
         cls.driver.find_element_by_name("password").send_keys("admin")
@@ -15,15 +18,47 @@ class TestLitecartMainPage(unittest.TestCase):
         cls.driver.find_element_by_name("login").click()
 
     def test_browser_logs(self):
+        wait = WebDriverWait(self.driver, 10)
         self.driver.find_element_by_xpath("//span[text()='Catalog']").click()
         self.driver.find_element_by_xpath("//a[text()='Rubber Ducks']").click()
-        self.driver.find_element_by_xpath("//a[text()='Subcategory']").click()
-        products_list = self.driver.find_elements_by_xpath("//tr[@class='row']/td[3]/a")
-        for product in products_list:
-            product.click()
-            wait.until(EC.text_to_be_present_in_element((By.XPATH, "//h1[@class='title' and text()='%s']"
-            self.driver.back
-        
+        # products_list = len(self.driver.find_elements_by_xpath("//tr[@class='row']/td[3]/a[contains(text(),'Duck')]"))
+        # for i in range(1, products_list):
+        #     self.driver.find_element_by_xpath("//tr[@class='row']/td[3]/a").click()
+        #     wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        #     print(self.driver.find_element_by_xpath("//td[@id='content']/h1").get_attribute("textContent"))
+        #     self.driver.back() 
+
+#================================================================================================================       
+        duck_element = self.driver.find_element_by_xpath("//a[text()='Blue Duck']")
+        duck_element.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        print(self.driver.get_log('browser'))
+        self.driver.back()
+
+        duck_element = self.driver.find_element_by_xpath("//a[text()='Green Duck']")
+        duck_element.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        print(self.driver.get_log('browser'))
+        self.driver.back()
+
+        duck_element = self.driver.find_element_by_xpath("//a[text()='Purple Duck']")
+        duck_element.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        print(self.driver.get_log('browser'))
+        self.driver.back()
+
+        duck_element = self.driver.find_element_by_xpath("//a[text()='Red Duck']")
+        duck_element.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        print(self.driver.get_log('browser'))
+        self.driver.back()
+
+        duck_element = self.driver.find_element_by_xpath("//a[text()='Yellow Duck']")
+        duck_element.click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
+        print(self.driver.get_log("browser"))
+        self.driver.back()
+#================================================================================================================
 
     # @classmethod
     # def tearDownClass(cls):
