f1b1f43cd09f515743f75a3b2ec5054eaa8db6d0
Łukasz Niewęgłowski <lukasz.nieweglowski86@gmail.com>
2019-03-28 19:40:51 +0100

    ex update

--- lesson10_ex17.py
+++ lesson10_ex17.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
 
 class TestLitecartMainPage(unittest.TestCase):
 
@@ -21,48 +20,55 @@ class TestLitecartMainPage(unittest.TestCase):
         wait = WebDriverWait(self.driver, 10)
         self.driver.find_element_by_xpath("//span[text()='Catalog']").click()
         self.driver.find_element_by_xpath("//a[text()='Rubber Ducks']").click()
-        # products_list = len(self.driver.find_elements_by_xpath("//tr[@class='row']/td[3]/a[contains(text(),'Duck')]"))
-        # for i in range(1, products_list):
-        #     self.driver.find_element_by_xpath("//tr[@class='row']/td[3]/a").click()
-        #     wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        #     print(self.driver.find_element_by_xpath("//td[@id='content']/h1").get_attribute("textContent"))
-        #     self.driver.back() 
-
-#================================================================================================================       
-        duck_element = self.driver.find_element_by_xpath("//a[text()='Blue Duck']")
-        duck_element.click()
+     
+        self.driver.find_element_by_xpath("//a[text()='Blue Duck']").click()
         wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        print(self.driver.get_log('browser'))
+        log_data = self.driver.get_log('browser')
+        if len(log_data) > 0:
+            print(log_data)
+        else:
+            print("No browser log.")
         self.driver.back()
 
-        duck_element = self.driver.find_element_by_xpath("//a[text()='Green Duck']")
-        duck_element.click()
+        self.driver.find_element_by_xpath("//a[text()='Green Duck']").click()
         wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        print(self.driver.get_log('browser'))
+        log_data = self.driver.get_log('browser')
+        if len(log_data) > 0:
+            print(log_data)
+        else:
+            print("No browser log.")
         self.driver.back()
 
-        duck_element = self.driver.find_element_by_xpath("//a[text()='Purple Duck']")
-        duck_element.click()
+        self.driver.find_element_by_xpath("//a[text()='Purple Duck']").click()
         wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        print(self.driver.get_log('browser'))
+        log_data = self.driver.get_log('browser')
+        if len(log_data) > 0:
+            print(log_data)
+        else:
+            print("No browser log.")
         self.driver.back()
 
-        duck_element = self.driver.find_element_by_xpath("//a[text()='Red Duck']")
-        duck_element.click()
+        self.driver.find_element_by_xpath("//a[text()='Red Duck']").click()
         wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        print(self.driver.get_log('browser'))
+        log_data = self.driver.get_log('browser')
+        if len(log_data) > 0:
+            print(log_data)
+        else:
+            print("No browser log.")
         self.driver.back()
 
-        duck_element = self.driver.find_element_by_xpath("//a[text()='Yellow Duck']")
-        duck_element.click()
+        self.driver.find_element_by_xpath("//a[text()='Yellow Duck']").click()
         wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@id='content']/h1")))
-        print(self.driver.get_log("browser"))
+        log_data = self.driver.get_log('browser')
+        if len(log_data) > 0:
+            print(log_data)
+        else:
+            print("No browser log.")
         self.driver.back()
-#================================================================================================================
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.driver.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
